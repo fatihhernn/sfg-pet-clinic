1098b38a4a4de0b8a34a5eb4e5e74a8752dd357d
fatih eren <fatihhernn@gmail.com>
2022-01-12 17:45:49 +0300

    Edited PetService Impl Map

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PetServiceMap.java
@@ -11,26 +11,26 @@ public class PetServiceMap extends AbstractMapService<Pet,Long> implements PetSe
 
     @Override
     public Set<Pet> findAll() {
-        return null;
+        return super.findAll();
     }
 
     @Override
     public void deleteById(Long id) {
-
+        super.deleteById(id);
     }
 
     @Override
-    public void delete(Pet object) {
-
+    public void delete(Pet pet) {
+        super.delete(pet);
     }
 
     @Override
-    public Pet save(Pet object) {
-        return null;
+    public Pet save(Pet pet) {
+        return super.save(pet);
     }
 
     @Override
     public Pet findById(Long id) {
-        return null;
+        return super.findById(id);
     }
 }
